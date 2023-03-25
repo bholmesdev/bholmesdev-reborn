438df629d1c0b1e49afa46a587741cddd0b6fa49
bholmesdev <bholmesdev@gmail.com>
2023-03-25 15:44:47 -0400

    chore: type-defs for simple-spring (open source contribution?)

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,23 @@
 /// <reference types="astro/client" />
+
+module "simple-spring" {
+  interface SpringOptions {
+    target: number;
+    value?: number;
+    tension?: number;
+    friction?: number;
+    mass?: number;
+    precision?: number;
+    fps?: number;
+
+    onFrame?: (value: number) => void;
+    onComplete?: () => void;
+    onStart?: () => void;
+    onRest?: () => void;
+  }
+
+  class Spring {
+    constructor(options: SpringOptions);
+    start(): void;
+  }
+}
