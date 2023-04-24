5aad14f6ef8b18d5e91219c4bf2e949a345ed7b1
bholmesdev <bholmesdev@gmail.com>
2023-04-24 16:12:20 -0400

    chore: update simple-spring types per PR

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,23 +1,37 @@
 /// <reference types="astro/client" />
 
-module "simple-spring" {
-  interface SpringOptions {
-    target: number;
-    value?: number;
+// PR to add types to core package under review
+// https://github.com/ryanking1809/simple-spring/pull/1
+declare module "simple-spring" {
+  export interface SpringOptions {
+    value?: number | number[];
+    target?: number | number[];
     tension?: number;
     friction?: number;
     mass?: number;
     precision?: number;
     fps?: number;
-
-    onFrame?: (value: number) => void;
-    onComplete?: () => void;
-    onStart?: () => void;
-    onRest?: () => void;
+    onStart?: ((value: number | number[], spring: Spring) => void) | null;
+    onFrame?: ((value: number | number[], spring: Spring) => void) | null;
+    onRest?: ((value: number | number[], spring: Spring) => void) | null;
+    onComplete?: ((value: number | number[], spring: Spring) => void) | null;
   }
 
-  class Spring {
-    constructor(options: SpringOptions);
-    start(): void;
+  export class Spring {
+    constructor(options?: SpringOptions);
+    set value(val: number | number[]);
+    get value(): number | number[];
+    getValue(): number | number[];
+    set target(val: number | number[]);
+    get target(): number | number[];
+    setTarget(val: number | number[]): this;
+    start(): this;
+    pause(): this;
+    stop(): this;
+    complete(): this;
+    tick(t?: number): this;
+    animateFrame(): void;
+    get restingAtTarget(): boolean;
+    get restingAtStart(): boolean;
   }
 }
