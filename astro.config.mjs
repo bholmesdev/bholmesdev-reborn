bcbb91a7507fd9796da7d3c2690d98bbcfe4b85b
bholmesdev <bholmesdev@gmail.com>
2023-07-04 13:47:42 -0400

    deps: bump everything

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import solidJs from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
-
 import markdoc from "@astrojs/markdoc";
+
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs(), tailwind(), markdoc()]
+  scopedStyleStrategy: "class",
+  integrations: [solidJs(), tailwind(), markdoc(), svelte()],
 });
